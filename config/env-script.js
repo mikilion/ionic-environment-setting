811bad27de7efe8ef0df25b98b6ac1a0c6d7dd89
Michele Liotino <mikilion@users.noreply.github.com>
2018-05-22 23:33:59 +0200

    Update env-script.js

--- config/env-script.js
+++ config/env-script.js
@@ -6,11 +6,10 @@ function readWriteSync() {
   var data = fs.readFileSync(`src/env/env.${env}.ts`, 'utf-8');
   fs.writeFileSync('src/env/env.ts', data, 'utf-8');
   
-  consoleOut();
+  consoleOut(env);
 }
 
-function consoleOut() {
-  let env = process.env.ENV || 'dev';
+function consoleOut(env) {
   let filePath = `src/env/env.${env}.ts`;
   
   console.log("============ PREPARE ENV FILE =============");
